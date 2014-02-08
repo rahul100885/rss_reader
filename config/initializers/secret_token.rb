64d094107ebcbb87d7ccd5df597dd2753d306f02
Rahul P. Chaudhari <rahul100885@gmail.com>
2014-02-08 22:20:32 +0530

    Added blank rails app with required gems

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RssReader::Application.config.secret_key_base = '5b0f188e81107d582ba365e97fed8a93fa7380b01d190f8fb13f59eb4d833b1589752ed4bdff0a50b002b8aa2b07a16e69f97868e6f9b7c97d72037fc2457f5d'
